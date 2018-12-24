853ccc6302ed673a835068d443937e244fa1b879
yuenandi <17610160611@163.com>
2018-12-24 21:07:35 +0800

    web转发

--- spring-web/spring-web-style/src/main/java/com/castor/web/style/StyleApplication.java
+++ spring-web/spring-web-style/src/main/java/com/castor/web/style/StyleApplication.java
@@ -2,6 +2,11 @@ package com.castor.web.style;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 public class StyleApplication {
